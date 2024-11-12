faeb1862e2672757617a02a463431fe224ae4797
Christian-Rosendal <mxchristian@hotmail.com>
2024-11-12 08:32:10 +0100

    Add files via upload

---
+++ LoraShieldkode.ino
@@ -0,0 +1,73 @@
+// MIT License
+// https://github.com/gonzalocasas/arduino-uno-dragino-lorawan/blob/master/LICENSE
+// Based on examples from https://github.com/matthijskooijman/arduino-lmic
+// Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
+
+// Adaptions: Andreas Spiess
+
+#include <lmic.h>
+#include <hal/hal.h>
+
+// OTAA værdier til LoRa modul
+static const u1_t APPEUI[8] = { 0x12, 0x34, 0x56, 0x66, 0x77, 0x88, 0x99, 0x10 };
+static const u1_t DEVEUI[8] = { 0x70, 0xB3, 0xD5, 0x7E, 0xD8, 0x00, 0x3A, 0x69 };
+static const u1_t APPKEY[16] = { 0xA3, 0x1D, 0xB0, 0x4A, 0x1C, 0x50, 0xC7, 0x58, 0x09, 0x6F, 0xEC, 0x75, 0x67, 0xD4, 0xC1, 0x2D };
+
+// Disse callbacks bruges til at hente OTAA-nøgler
+void os_getArtEui (u1_t* buf) { memcpy(buf, APPEUI, 8); }
+void os_getDevEui (u1_t* buf) { memcpy(buf, DEVEUI, 8); }
+void os_getDevKey (u1_t* buf) { memcpy(buf, APPKEY, 16); }
+
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty cycle limitations).
+const unsigned TX_INTERVAL = 660;
+
+// Pin mapping Dragino Shield
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+void onEvent (ev_t ev) {
+    if (ev == EV_TXCOMPLETE) {
+        Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+        // Schedule next transmission
+        os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+    }
+}
+
+void do_send(osjob_t* j){
+    // Payload to send (uplink)
+    static uint8_t message[] = "hi";
+
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, message, sizeof(message)-1, 0);
+        Serial.println(F("Sending uplink packet..."));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("Starting..."));
+
+    // LMIC init
+    os_init();
+
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Start job
+    do_send(&sendjob);
+}
+
+void loop() {
+    os_runloop_once();
+}
