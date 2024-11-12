20bcb327aadaa9dce717f1d1a65a34c29758bd5f
shza <124167962+shza18@users.noreply.github.com>
2024-11-12 11:43:09 +0100

    Update readVoltage.ino

--- readVoltage.ino
+++ readVoltage.ino
@@ -3,7 +3,7 @@ int d1_R1 = 2200000 // resistor name: divider#nr_R#nr
 int d1_R2 = 8800000
 int measureVolt_analogPin = 0;
 
-//Get the current as measured using the voltage divider circuit;
+//Get the battery voltage, measured using the voltage divider circuit;
 float readVoltage(int R1, int R2, int analogPin) {
   // analogRead values =(0-1023)=(0V-5V), coresponding to 205.8 "point" pr. 1V voltage increase;
   float measuredVoltage = analogRead(analogPin) / (1023/5);
